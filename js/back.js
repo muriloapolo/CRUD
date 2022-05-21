75d6aa3ed2ab849abcf2f3d46c177d358ac80d08
muriloapolo <massps2@gmail.com>
2022-05-21 16:43:31 -0300

    Crud completo dentro dos meus conhecimentos

--- js/back.js
+++ js/back.js
@@ -1,82 +1,65 @@
-    const dados = [{
-            nome: "Murilo",
-            sobrenome: "Apolonio",
-            telefone: 999999,
-            email: 'asasas@asasas.asa'
-        },
-        //     {
-        //         nome: "wewwe",
-        //         sobrenome: "Apolonio",
-        //         telefone: 999999,
-        //         email: 'asasas@asasas.asa'
-        //     },
-        //     {
-        //         nome: "fsfsf",
-        //         sobrenome: "Apolonsdsdasdio",
-        //         telefone: 999999,
-        //         email: 'asasas@asasas.asa'
-        //     },
-        //     {
-        //         nome: "sdasdasdasdasdasd",
-        //         sobrenome: "asdasd",
-        //         telefone: 999999,
-        //         email: 'asasas@asasas.asa'
-        //     },
-        //     {
-        //         nome: "Muasdacxzczxczxczxcrilo",
-        //         sobrenome: "Apolonio",
-        //         telefone: 999999,
-        //         email: 'asasas@asasas.asa'
-        //     },
+window.addEventListener('DOMContentLoaded', () => {
 
-    ];
-
-    //Seleciona o formulario para enviar os dados
-    const confirma = document.querySelector('.confirma');
-    confirma.addEventListener('click', insereDados);
-    //Captura o local aonde os dados serão inseridos
-    const tbody = document.querySelector('tbody');
-
-
-
-
-
-    function insereDados() {
-        //Limpa a tela e preenche posteriormente com o Preenche Dados
-        tbody.innerHTML = ""
-        let n = document.querySelector('.nome');
-        let s = document.querySelector('.sobrenome');
-        let t = document.querySelector('.telefone');
-        let e = document.querySelector('.email');
-
-        dados.push({
-            'nome': n.value,
-            'sobrenome': s.value,
-            'telefone': t.value,
-            'email': e.value
-        });
-
-        //Executa o preenchimento dos dados
+    let s = localStorage.getItem('dados');
+    if (s) {
+        dados = JSON.parse(s);
         preencheDados();
-
-        //Limpa os campos
-        n.value = '';
-        s.value = '';
-        t.value = '';
-        e.value = '';
     }
+});
+const limpaDadosGeral = document.getElementById('limpaTudo');
+limpaDadosGeral.addEventListener('click', function () {
+    let limpa = confirm('Limpar Todos os Dados?'); {
+        if (limpa) {
+            localStorage.clear();
+            window.location.reload();
+        }
+    }
+})
+let dados = new Array();
 
- 
+
+//Seleciona o formulario para enviar os dados
+const confirma = document.querySelector('.confirma');
+confirma.addEventListener('click', insereDados);
+//Captura o local aonde os dados serão inseridos
+const tbody = document.querySelector('tbody');
+
+function insereDados() {
+    //Limpa a tela e preenche posteriormente com o Preenche Dados
+    tbody.innerHTML = ""
+    let n = document.querySelector('.nome');
+    let s = document.querySelector('.sobrenome');
+    let t = document.querySelector('.telefone');
+    let e = document.querySelector('.email');
+
+    dados.push({
+        'nome': n.value,
+        'sobrenome': s.value,
+        'telefone': t.value,
+        'email': e.value
+    });
+
+    //Executa o preenchimento dos dados
+    preencheDados();
+    salvar();
+    //Limpa os campos
+    n.value = '';
+    s.value = '';
+    t.value = '';
+    e.value = '';
+}
 
 
-    function preencheDados() {
-        tbody.innerHTML = ''
-        //Cria o identificador do Array
 
-        let linha = 0;
-        dados.forEach((d) => {
-            tbody.innerHTML +=
-                `<tr >
+
+function preencheDados() {
+    tbody.innerHTML = ''
+    //Cria o identificador do Array
+
+    let linha = 0;
+    dados.forEach((d) => {
+        tbody.innerHTML +=
+            `<tr >
                 <td class="n">${d.nome}</td>
                 <td class="s">${d.sobrenome}</td>
                 <td class="t">${d.telefone}</td>
@@ -86,74 +69,91 @@
                     <button type="button" class="deletar" data-indexArray="${linha}"> D</button>
                     </td>
                     </tr>`
-            linha++
+        linha++
+    });
+    updateDados()
+}
+
+let dataSet;
+
+
+function updateDados() {
+    const updateBtn = document.querySelectorAll('.editar');
+    updateBtn.forEach((btnU) => {
+        btnU.addEventListener("click", function () {
+            //Captura o indice do botão;
+            dataSet = parseInt(btnU.dataset.indexarray);
+            configuraTela.classList.add('pop-modal');
+            tabela.style.display = 'none';
+            editaCampo();
         });
-        updateDados()
-    }
+    });
 
-    let dataSet;
-
-
-    function updateDados() {
-        const updateBtn = document.querySelectorAll('.editar');
-        updateBtn.forEach((btnU) => {
-            btnU.addEventListener("click", function () {
-                //Captura o indice do botão;
-                dataSet = parseInt(btnU.dataset.indexarray);
-                editaCampo();
-            });
+    const deleteBtn = document.querySelectorAll('.deletar');
+    deleteBtn.forEach((btnD) => {
+        btnD.addEventListener("click", function () {
+            //Captura o indice do botão;
+            dataSet = parseInt(btnD.dataset.indexarray);
+            deletaCampo();
         });
-        const deleteBtn = document.querySelectorAll('.deletar');
-        deleteBtn.forEach((btnD) => {
-            btnD.addEventListener("click", function () {
-                //Captura o indice do botão;
-                dataSet = parseInt(btnD.dataset.indexarray);
-                deletaCampo();
-            });
-        });
+    });
+
+}
+
+const formEditar = document.querySelector('.modal-edita').children;
+
+let nE = formEditar[0];
+let sE = formEditar[1];
+let tE = formEditar[2];
+let eE = formEditar[3];
+let confirmaEdicao = document.querySelector('.edita');
+
+function editaCampo() {
+
+    nE.value = dados[dataSet].nome;
+    sE.value = dados[dataSet].sobrenome;
+    tE.value = dados[dataSet].telefone;
+    eE.value = dados[dataSet].email;
+    confirmaEdicao.addEventListener('click', confirmaEditar);
+}
+
+function confirmaEditar() {
+
+    dados[dataSet] = {
+        'nome': nE.value,
+        'sobrenome': sE.value,
+        'telefone': tE.value,
+        'email': eE.value
     }
+    nE.value = '';
+    sE.value = '';
+    tE.value = '';
+    eE.value = '';
+    configuraTela.classList.remove('pop-modal');
+    tabela.style.display = 'flex';
+    preencheDados();
+    salvar();
+}
 
-    const formEditar = document.querySelector('.modal-edita').children;
-
-    let nE = formEditar[0];
-    let sE = formEditar[1];
-    let tE = formEditar[2];
-    let eE = formEditar[3];
-    let confirmaEdicao = document.querySelector('.edita');
-
-    function editaCampo() {
-
-        nE.value = dados[dataSet].nome;
-        sE.value = dados[dataSet].sobrenome;
-        tE.value = dados[dataSet].telefone;
-        eE.value = dados[dataSet].email;
-        confirmaEdicao.addEventListener('click', confirmaEditar);
-    }
-
-    function confirmaEditar() {
-
-        dados[dataSet] = {
-            'nome': nE.value,
-            'sobrenome': sE.value,
-            'telefone': tE.value,
-            'email': eE.value
-        }
-        nE.value = '';
-        sE.value = '';
-        tE.value = '';
-        eE.value = '';
-
+function deletaCampo() {
+    let confirmarDeletar = confirm('Tem certeza?');
+    if (confirmarDeletar) {
+        dados.splice(dataSet, 1);
         preencheDados();
-
-
-    }
-
-    function deletaCampo() {
-        let confirmarDeletar = confirm('Tem certeza?');
-        if (confirmarDeletar) {
-            dados.splice(dataSet, 1);
-            preencheDados();
-        } else {
-            preencheDados();
+        salvar();
+        if (dados.length === 0) {
+            localStorage.clear();
+            console.log(dados.length)
         }
+    } else {
+        preencheDados();
     }
+}
+
+/* Gravar na mémoria LOCAL/STORAGE */
+function salvar() {
+    localStorage.clear();
+    let d = JSON.stringify(dados);
+    console.log(d);
+    localStorage.setItem('dados', d);
+}
--- js/front.js
+++ js/front.js
@@ -2,11 +2,13 @@
     const insereModal = document.getElementById('insere');
     const tabela = document.querySelector('.tabela-center');
 
+
     insereModal.addEventListener('click', carregaModal)
 
 
 
     //Função para Carregar o modal de inserção na tela
+
     function carregaModal() {
         //container-central
         //modal-insere
@@ -19,43 +21,30 @@
         fecharModal.addEventListener('click', () => {
             modal.classList.remove('pop-modal');
             tabela.style.display = 'flex'
-           
+
         })
         const btnModal = modal.querySelector('.confirma');
 
         btnModal.addEventListener('click', confirmacao);
 
         function confirmacao() {
-            editarModal()
             let confirma = modal.querySelector('.textConfirma');
             confirma.style.display = 'block';
             setTimeout(() => {
                 confirma.style.display = 'none';
-            }, 850);
-
-
+                modal.classList.remove('pop-modal');
+            tabela.style.display = 'flex'
+            }, 150);
+            
         }
     }
 
 
-    /*----------------------EDITAR  dados modal -----------------------*/
+    //Editar modal
     const configuraTela = document.querySelector('.configsAparencia');
-    console.log(configuraTela);
-    function editarModal(){
-        let botoesEditar = document.querySelectorAll('.editar');
-        console.log(botoesEditar);
-        botoesEditar.forEach((bE)=>{
-            bE.addEventListener('click',function(){
-                configuraTela.classList.add('pop-modal');
-                tabela.style.display = 'none';
-            })
-        });
-        const fecharModal = configuraTela.querySelector('.close-btn');
-        fecharModal.addEventListener('click', () => {
-            configuraTela.classList.remove('pop-modal');
-            tabela.style.display = 'flex'
-           
-        })
-    }
-
-    /*--------------------------------------------------------------*/
+    let btnEditaCampo = document.getElementById("editado");
+    const fecharModal = configuraTela.querySelector('.close-btn');
+    fecharModal.addEventListener('click', () => {
+        configuraTela.classList.remove('pop-modal');
+        tabela.style.display = 'flex';
+    });
